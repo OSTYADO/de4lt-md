ef42347451f3e7cb3cf934f1805a6a147dd66fd2
OSTYADO <clamourostyado@gmail.com>
2024-12-03 20:45:47 +0300

    Update ping.js

--- Cmds/General/ping.js
+++ Cmds/General/ping.js
@@ -1,5 +1,5 @@
 module.exports = async (context) => {
-        const { client, m, de4ltmdspeed } = context;
+        const { client,m,de4ltmdspeed } = context;
 
 
 await m.reply(`Pong\n${de4ltmdspeed.toFixed(4)}ms`)
